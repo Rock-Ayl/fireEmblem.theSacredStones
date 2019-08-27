6cf41a066262577f8b8224353b33fdb52e5b6eb3
anyongliang <anyongliang@icreations.com.cn>
2019-08-27 11:04:24 +0800

    修改：注释

--- src/main/java/start/Main.java
+++ src/main/java/start/Main.java
@@ -21,17 +21,18 @@ public class Main {
         frame.setSize(400, 300);
         //设置界面是否可见
         frame.setVisible(true);
+        //设置用户单击窗口的关闭按钮时程序执行的操作(关系窗口)
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //设置键盘监听器
         frame.addKeyListener(new KeyInput());
 
 
         //增加一个光标线程1
-        Cursor cur = new Cursor(frame,500,0,0,32,32);
+        Cursor cur = new Cursor(frame, 500, 0, 0, 32, 32);
         new Thread(cur).start();
 
         //增加一个光标线程2
-        Cursor cur2 = new Cursor(frame,1000,30,30,32,32);
+        Cursor cur2 = new Cursor(frame, 1000, 30, 30, 32, 32);
         new Thread(cur2).start();
 
     }
