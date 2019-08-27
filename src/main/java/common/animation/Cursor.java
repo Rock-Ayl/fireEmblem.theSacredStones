18d99791268de8b2a235649eace112d62f7199e3
anyongliang <anyongliang@icreations.com.cn>
2019-08-27 10:31:58 +0800

    新增：一个光标实体(半完成)

--- src/main/java/common/animation/Cursor.java
+++ src/main/java/common/animation/Cursor.java
@@ -1,4 +1,84 @@
 package common.animation;
 
-public class Cursor {
+import common.Const;
+import common.util.FileUtil;
+
+import javax.swing.*;
+
+/**
+ * created by Rock-Ayl on 2019-8-27
+ * 一个光标实体
+ */
+public class Cursor implements Runnable {
+
+    //光标放置的面板
+    JFrame frame;
+    //光标对象
+    JLabel label;
+    //光标图标编号
+    int num = 1;
+    //光标刷新时间
+    int time;
+    //光标x轴坐标
+    int x;
+    //光标y轴坐标
+    int y;
+    //光标宽
+    int w;
+    //光标高
+    int h;
+
+    //初始化光标线程对象
+    public Cursor(JFrame frame, int time, int x, int y, int w, int h) {
+        this.time = time;
+        this.frame = frame;
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.label = VOIDCursor();
+        frame.add(this.label);
+
+    }
+
+    //创建一个光标
+    private JLabel VOIDCursor() {
+        //找到当前编号的光标图片
+        ImageIcon img = new ImageIcon(FileUtil.setNumForFile(Const.CursorPath, num));
+        //编号++
+        num++;
+        if (num >= 9) {
+            num = 1;
+        }
+        //组件
+        JLabel cursor = new JLabel(img);
+        //设置水平对齐
+        cursor.setHorizontalAlignment(SwingConstants.CENTER);
+        //设置垂直对齐
+        cursor.setVerticalAlignment(SwingConstants.CENTER);
+        //设置组件 x y 轴  宽高
+        cursor.setBounds(x, y, w, h);
+        //返回光标
+        return cursor;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                //光标刷新延迟
+                Thread.sleep(time);
+                //删除旧光标
+                frame.remove(label);
+                //新光爆
+                label = VOIDCursor();
+                //放入面板
+                frame.add(label);
+                //刷新面板
+                frame.setVisible(true);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/start/Main.java
+++ src/main/java/start/Main.java
@@ -1,6 +1,7 @@
 package start;
 
 import common.Const;
+import common.animation.Cursor;
 import common.input.KeyInput;
 
 import javax.swing.*;
@@ -12,27 +13,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ImageIcon img = new ImageIcon(Const.CursorPath);
-        //组件
-        JLabel label = new JLabel(img);
-        //设置水平对齐
-        label.setHorizontalAlignment(SwingConstants.CENTER);
-        //设置垂直对齐
-        label.setVerticalAlignment(SwingConstants.CENTER);
-        //设置组件 x y 轴  宽高
-        label.setBounds(0, 0, 128, 128);
-
-        //容器
-        JPanel panel = new JPanel();
-        //组件添加标签
-        panel.add(label);
-        //组件布局
-        panel.setLayout(null);
 
         //创建界面-边框及标题
         JFrame frame = new JFrame("火焰之纹章-圣光之魔石");
-        //界面添加内容面板
-        frame.getContentPane().add(panel);
         frame.pack();
         //设置界面宽高
         frame.setSize(400, 300);
@@ -42,6 +25,14 @@ public class Main {
         //设置键盘监听器
         frame.addKeyListener(new KeyInput());
 
+
+        //增加一个光标线程1
+        Cursor cur = new Cursor(frame,500,0,0,32,32);
+        new Thread(cur).start();
+
+        //增加一个光标线程2
+        Cursor cur2 = new Cursor(frame,1000,30,30,32,32);
+        new Thread(cur2).start();
 
     }
 
