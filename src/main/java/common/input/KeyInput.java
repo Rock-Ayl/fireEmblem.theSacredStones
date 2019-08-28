7b4bc2e9822f3cc514953924457a1a67d068b81f
anyongliang <anyongliang@icreations.com.cn>
2019-08-28 16:21:42 +0800

    修改：注释

--- src/main/java/common/input/KeyInput.java
+++ src/main/java/common/input/KeyInput.java
@@ -20,51 +20,56 @@ public class KeyInput implements KeyListener {
     public void keyPressed(KeyEvent e) {
         int keyCode = e.getKeyCode();
         System.out.println("输入:" + keyCode);
-        /**
-         * 上:38
-         * 下:40
-         * 左:37
-         * 右:39
-         * 回车:10
-         * ESC:27
-         * z:90
-         * x:88
-         * c:67
-         * @param e
-         */
         switch (keyCode) {
+            //回车
+            case 10:
+                break;
+            //ESC
+            case 27:
+                break;
+            //左
             case 37:
                 Main.CharacterCursor.moveLeft();
                 break;
+            //上
             case 38:
                 Main.CharacterCursor.moveUp();
                 break;
+            //右
             case 39:
                 Main.CharacterCursor.moveRight();
                 break;
+            //下
             case 40:
                 Main.CharacterCursor.moveDown();
                 break;
+            //z
             case 90:
                 Main.CharacterCursor.start();
                 break;
+            //x
             case 88:
                 Main.CharacterCursor.stop();
                 break;
+            //c
             case 67:
                 Main.CharacterCursor.clear();
                 break;
+            //a
             case 65:
                 Main.CharacterCursor.switchType(0);
                 break;
+            //s
             case 83:
                 Main.CharacterCursor.switchType(1);
                 break;
+            //d
             case 68:
                 Main.CharacterCursor.switchType(2);
                 break;
+            //q
             case 81:
-                Main.CharacterCursor.switchXY(30,50);
+                Main.CharacterCursor.switchXY(30, 50);
                 break;
         }
     }
